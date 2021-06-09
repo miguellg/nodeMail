8b06915ff4404b4a8e7b6916248f07a49c62f1cd
Miguel L. Garcia <miguellg88@gmail.com>
2021-06-09 14:52:28 -0300

    MailBkp

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 var nodemailer = require('nodemailer');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
 require('dotenv').config();
 
-var remetente = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: process.env.MAILUSER,
-		pass: process.env.MAILPASS
+function sendMail(info, file){
+	var remetente = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.MAILUSER,
+			pass: process.env.MAILPASS
+		}
+	});
+
+	
+	let hoje = file.substring(7,17)
+
+	var email = {
+		from: process.env.FROM,
+		to: info.destino,
+		subject: 'Backup Sistema',
+		text: 'Backup do sistema realizado em '+hoje,
+		attachments: [{
+			filename: file,
+			path: info.path+'/'+file
+		}]
+	};
+
+	remetente.sendMail(email, function(error){
+		if (error) {
+	        	console.log(error);
+		} else {
+			console.log('Email enviado com sucesso.');
+		}
+	});	
+}
+
+async function getBkp(info){	
+	let files = await fsPromises.readdir(info.path)
+	
+	sendMail(info, files.pop())
+}
+
+fs.readFile("./config.json" , "utf8", function(err, data){
+	let json = JSON.parse(data)
+	for(let i in json){
+		await getBkp(json[i])
 	}
-});
-
-let data = new Date();
-let hoje = data.getDate()+'/'+(data.getMonth()+1).toString().padStart(2,'0')+'/'+data.getFullYear()
-
-var email = {
-	from: process.env.FROM,
-	to: process.env.DEST,
-	subject: 'Backup Sistema',
-	text: 'Backup do sistema realizado em '+hoje,
-	attachments: [{
-  		filename: 'backup-'+(data.getFullYear()+((data.getMonth()+1).toString().padStart(2,'0'))+data.getDate())+'.sql',
-		path: '/caminho/arquivo.txt'
-	}]
-};
-
-remetente.sendMail(email, function(error){
-	if (error) {
-        	console.log(error);
-	} else {
-		console.log('Email enviado com sucesso.');
-	}
-});	
+})
